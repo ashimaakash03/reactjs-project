fbff5945baf861eac2d64510daf1e69642abf299
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-05 11:26:50 +0530

    demonstrated dynamic values in JSX

--- reactjs-project/src/components/Hello.jsx
+++ reactjs-project/src/components/Hello.jsx
@@ -1,6 +1,12 @@
 function Hello() {
+  const name = "Ashim Aakash";
+  const age = 26;
 
-    return <h2>Message from Hello Component...</h2>
+  return (
+    <h2>
+      Hi, my name is {name} and I am {age} years old...
+    </h2>
+  );
 }
 
 export default Hello;
