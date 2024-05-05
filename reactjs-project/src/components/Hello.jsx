16400d7248ff4e4dd599c0f6d10c7ea698139d7c
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-05 13:02:13 +0530

    created function and displayed message in Hello component

--- reactjs-project/src/components/Hello.jsx
+++ reactjs-project/src/components/Hello.jsx
@@ -3,10 +3,17 @@ function Hello() {
   const age = 26;
 
   return (
-    <h2>
-      Hi, my name is {name} and I am {age} years old...
-    </h2>
+    <>
+      <div className="message">Greet Message: {greetMessage()}</div>
+      <h2>
+        Hi, my name is {name} and I am {age} years old...
+      </h2>
+    </>
   );
+}
+
+function greetMessage() {
+  return "Greetings!!! Now learning ReactJS";
 }
 
 export default Hello;
