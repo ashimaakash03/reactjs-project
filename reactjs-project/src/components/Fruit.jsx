e13a3b32253107b30bb5a4c57ca78eb6b68a3dbb
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-06 16:59:54 +0530

    modified Fruit component file

--- reactjs-project/src/components/Fruit.jsx
+++ reactjs-project/src/components/Fruit.jsx
@@ -2,26 +2,10 @@
 const Fruit = ({ name, price }) => {
   // eslint-disable-next-line react/prop-types
   return (
-<<<<<<< HEAD
     <>
-=======
-    <div>
->>>>>>> f41be58c51e533645c911779369b1e5b43fe2825
       {/*eslint-disable-next-line react/prop-types*/}
-      {/* Name: {name}. Price: ${price} */}
-
-      {price >= 14 ? (
-        <li>
-          Name: {name}. Price: ${price}{" "}
-        </li>
-      ) : (
-        " "
-      )}
-<<<<<<< HEAD
+      Name: {name}. Price: ${price}
     </>
-=======
-    </div>
->>>>>>> f41be58c51e533645c911779369b1e5b43fe2825
   );
 };
 
