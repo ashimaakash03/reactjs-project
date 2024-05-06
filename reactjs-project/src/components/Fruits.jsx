24cba42b848f1bd96aa22e84ed8470a861a234fd
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-06 11:39:21 +0530

    modified simple fruits array to array of fruits object

--- reactjs-project/src/components/Fruits.jsx
+++ reactjs-project/src/components/Fruits.jsx
@@ -1,12 +1,19 @@
 const Fruits = () => {
-  const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"];
+  //const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"];
+  const fruits = [
+    { name: "Apple", price: 10 },
+    { name: "Mango", price: 12 },
+    { name: "Banana", price: 14 },
+    { name: "Orange", price: 16 },
+    { name: "Pineapple", price: 18 },
+  ];
   return (
     <div>
-      <ul>
+      {/* <ul>
         {fruits.map((fruit) => (
           <li key={fruit}>{fruit}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
