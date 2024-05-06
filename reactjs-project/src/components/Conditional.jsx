aeb3e9f315f59d267a1661079c1bb9ff65570c57
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-06 14:06:26 +0530

    rendered conditional JSX using if else statement

--- reactjs-project/src/components/Conditional.jsx
+++ reactjs-project/src/components/Conditional.jsx
@@ -1,10 +1,19 @@
 const Conditional = () => {
-  return (
-    <div>
-      <h3>This statement must be rendered if condition is true</h3>
-      <h3>This statement must be rendered if the condition is false</h3>
-    </div>
-  );
+  const condition = false;
+
+  if (condition) {
+    return (
+      <div>
+        <h3>This statement must be rendered if condition is true</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h3>This statement must be rendered if the condition is false</h3>
+      </div>
+    );
+  }
 };
 
 export default Conditional;
